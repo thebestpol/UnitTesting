f71feaea4ca0e342db894c88b379bb4b09cd3afd
Pol Gómez <pol.gomez.external@worldline.com>
2016-04-19 16:45:39 +0200

    Renaming sales op test class and added some comments

--- app/src/test/java/com/kata/unittesting/BasketTest.java
+++
@@ -1,8 +0,0 @@
-package com.kata.unittesting;
-
-/**
- * BasketTest
- */
-public class BasketTest {
-
-}
---
+++ app/src/test/java/com/kata/unittesting/SalesOperativeTest.java
@@ -0,0 +1,10 @@
+package com.kata.unittesting;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ * <p>
+ * Here we will test the sales operative collaborators, know as sociable tests
+ */
+public class SalesOperativeTest {
+
+}
